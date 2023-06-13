26560f0d82397b5c882476f0f04671c8a348fafb
Samuel Darkwah Manu <manusam14@yahoo.com>
2023-06-13 13:15:59 +0200

    Add map

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,4 +1,18 @@
 <script setup>
+import { onMounted } from 'vue'
+import "leaflet/dist/leaflet.css"
+import L from "leaflet"
+
+
+onMounted(() => {
+  const map = L.map('map').setView([47.391, 13.788], 8);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+})
+
 
 </script>
 
