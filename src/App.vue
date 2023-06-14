c75180b34c91017ff5ee623f3bc873ea62e45f45
ulrike99 <ulrike-holfeld@gmx.de>
2023-06-14 16:54:46 +0200

    title

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import InfoBox from './components/InfoBox.vue';
 <template>
   <header>
     <h1>Austrian Castles</h1>
-    <img alt="Vue logo" class="logo" src="@/assets/fortress.svg" width="50" height="50" />
+    <img alt="fortress" class="logo" src="@/assets/fortress.svg" width="50" height="50" />
   </header>
   <MapComponent />
   <InfoBox />
