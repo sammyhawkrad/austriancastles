9f8bd9d0fff2b3b2c5d6015ad5eb6a48f37dd0da
ulrike99 <ulrike-holfeld@gmx.de>
2023-06-17 12:23:27 +0200

    add about-button

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -3,7 +3,18 @@
 </script>
 
 <template>
-  <div id="about"></div>
+  <div id="about-button">
+  <img class="about" alt="about" src="@/assets/about.svg"/>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#about-button {
+  position: absolute;
+  z-index: 2;
+  width: 40px;
+  height: 40px;
+  left: 1%;
+  top: 87%;
+}
+</style>
