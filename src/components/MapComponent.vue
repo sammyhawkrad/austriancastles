a87b4fa64228f34a5298d6610115e0684659a746
Samuel Darkwah Manu <manusam14@yahoo.com>
2023-06-19 14:09:31 +0200

    load data from geoserver

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,20 +1,38 @@
 <script setup>
-import { onMounted } from 'vue'
-import "leaflet/dist/leaflet.css"
-import L from "leaflet"
+import { onMounted } from 'vue';
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+
+const geoserver = 'http://geoserver--vxkp129.bluemoss-ee5ab993.westus2.azurecontainerapps.io/geoserver/lbs/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=lbs%3Aaustriancastles&outputFormat=application%2Fjson'
+
 
 
 onMounted(() => {
   const map = L.map('map').setView([47.691, 13.388], 8);
-  map.setMaxBounds(map.getBounds());
+  map.fitBounds([[49.824, 19.666],[46.453, 8.498]]);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    minZoom: 8,
+    minZoom: 7,
   }).addTo(map);
 
   L.control.scale({position:'bottomleft', metric: true, imperial: false}).addTo(map);
+
+
+  const loadData = async () => {
+    const response = await fetch(geoserver);
+    const data = await response.json();
+    return data
+  }
+
+  const addCastles = async () => {
+    const castles = await loadData();
+    L.geoJSON(castles).addTo(map);
+
+  }
+  addCastles();
 
   // L.Control.geocoder({
   //   geocoder: L.Control.Geocoder.nominatim({
